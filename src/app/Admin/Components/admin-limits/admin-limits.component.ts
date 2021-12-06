289cf26ff84b17a87a4edd4ee9aad951c5c91fa7
Mona Elshikh <mona.elshikh@hotmail.com>
2021-12-06 11:34:03 +0200

    admin-settings-page

--- src/app/Admin/Components/admin-limits/admin-limits.component.ts
+++ src/app/Admin/Components/admin-limits/admin-limits.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { appLimits } from 'Shared/models/LimitsAndUpgrade';
+import { LimitsAndUpgradeService } from 'Shared/Services/limits-upgrade.service';
 
 @Component({
   selector: 'app-admin-limits',
   templateUrl: './admin-limits.component.html',
-  styleUrls: ['./admin-limits.component.css']
+  styleUrls: ['./admin-limits.component.css'],
 })
-export class AdminLimitsComponent implements OnInit {
-
-  constructor() { }
+export class AdminLimitsComponent implements OnInit, OnDestroy {
+  limits: appLimits = {} as appLimits;
+  limitsObject: appLimits = {} as appLimits;
+  LimitsSubscription: Subscription;
+  constructor(private LimitsAndUpgradeService: LimitsAndUpgradeService) {}
 
   ngOnInit(): void {
+    this.getAllLimits();
   }
+  ngOnDestroy() {
+    if (this.LimitsSubscription) this.LimitsSubscription.unsubscribe();
+  }
+  async getAllLimits() {
+    this.LimitsSubscription = (
+      await this.LimitsAndUpgradeService.getAll()
+    ).subscribe((result: any) => {
+      if (result) {
+        this.limits = result[0];
+        console.log(
+          'Limits From settings >>' + this.limits.personalPremiumRequestLimit
+        );
+      }
+    });
+  }
+  async SaveLimits(form: NgForm) {
+    this.limitsObject.id = this.limits.id;
+    this.limitsObject.personalRegularRequestLimit =
+      this.limits.personalRegularRequestLimit;
+    this.limitsObject.personalPremiumRequestLimit =
+      this.limits.personalPremiumRequestLimit;
+    this.limitsObject.personalFeaturedRequestLimit =
+      this.limits.personalFeaturedRequestLimit;
 
+    this.limitsObject.personalRegularArticleLimit =
+      this.limits.personalRegularArticleLimit;
+    this.limitsObject.personalPremiumArticleLimit =
+      this.limits.personalPremiumArticleLimit;
+    this.limitsObject.personalFeaturedArticleLimit =
+      this.limits.personalFeaturedArticleLimit;
+
+    this.limitsObject.regularalphotosNo = this.limits.regularalphotosNo;
+    this.limitsObject.premiumphotosNo = this.limits.premiumphotosNo;
+    this.limitsObject.featuredphotosNo = this.limits.featuredphotosNo;
+
+    //print values
+    console.log(`id:  ${this.limitsObject.id}
+                 personalRegularRequestLimit: ${this.limitsObject.personalRegularRequestLimit}
+                 personalPremiumRequestLimit: ${this.limitsObject.personalPremiumRequestLimit}
+                 personalFeaturedRequestLimit: ${this.limitsObject.personalFeaturedRequestLimit}`);
+
+    this.LimitsSubscription = (
+      await this.LimitsAndUpgradeService.create(this.limitsObject)
+    ).subscribe((result: any) => {
+      if (result) {
+        console.log(`saved`);
+      }
+    });
+  }
 }
--- src/app/Shared/Services/limits-upgrade.service.ts
+++ src/app/Shared/Services/limits-upgrade.service.ts
@@ -4,25 +4,36 @@ import { DataService } from 'Shared/Services/data.service';
 import { environment } from '../../../environments/environment';
 import { LocalstorageService } from 'Shared/Services/local-storage.service';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LimitsAndUpgradeService extends DataService {
-  constructor(http: HttpClient, private _http: HttpClient,localStorage:LocalstorageService) {
-    super(http, environment.BASE_URL + "/AdminSettings",localStorage);
+  constructor(
+    http: HttpClient,
+    private _http: HttpClient,
+    localStorage: LocalstorageService
+  ) {
+    super(http, environment.BASE_URL + '/AdminSettings', localStorage);
   }
   GetProfilePaymentInfo(profileId) {
-    return this._http.get(environment.BASE_URL + '/ProfilePayment/' + profileId);
+    return this._http.get(
+      environment.BASE_URL + '/ProfilePayment/' + profileId
+    );
   }
-  InsertUpdateProfilePaymentInfo(paymentInfo:any){
-    return this._http.post(environment.BASE_URL + '/ProfilePayment' , paymentInfo);
+  InsertUpdateProfilePaymentInfo(paymentInfo: any) {
+    return this._http.post(
+      environment.BASE_URL + '/ProfilePayment',
+      paymentInfo
+    );
   }
   GetUpgradeHistory(profileId: any) {
-    return this._http.get(environment.BASE_URL + '/UpgradeHistory/' + profileId);
+    return this._http.get(
+      environment.BASE_URL + '/UpgradeHistory/' + profileId
+    );
   }
-  GetMemberShipDetails(){
+  GetMemberShipDetails() {
     return this._http.get(environment.BASE_URL + '/MemberShipDetails');
   }
-  RequestUpgrade(upgrade:any){
-    return this._http.post(environment.BASE_URL + '/RequestUpgrade' , upgrade);
+  RequestUpgrade(upgrade: any) {
+    return this._http.post(environment.BASE_URL + '/RequestUpgrade', upgrade);
   }
 }
